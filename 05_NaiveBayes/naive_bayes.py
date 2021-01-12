e20e40088785b05d610246adb89b3382a36943fe
KayKozaronek <kozaronek@gmail.com>
2021-01-12 15:18:14 +0100

    I worked on an implementation on the Naive Bayes algorithm and a file to test the implementation

---
+++ 05_NaiveBayes/naive_bayes.py
@@ -0,0 +1,47 @@
+import numpy as np
+
+# Explanation of Naive Bayes:
+# https://towardsdatascience.com/naive-bayes-classifier-81d512f50a7c
+
+class NaiveBayes:
+
+    def fit(self, X, y):
+        n_samples, n_features = X.shape
+        self._classes = np.unique(y)
+        n_classes = len(self._classes)
+
+        # init mean, var, priors
+        self._mean = np.zeros((n_classes, n_features), dtype=np.float64)
+        self._var = np.zeros((n_classes, n_features), dtype = np.float64)
+        self._priors = np.zeros(n_classes, dtype=np.float64)
+
+        for idx, c in enumerate(self._classes):
+            X_c = X[c == y]
+            self._mean[idx, :] = X_c.mean(axis = 0)
+            self._var[idx, :] = X_c.var(axis = 0)
+            self._priors[idx] = X_c.shape[0] / float(n_samples)
+
+
+    def predict(self, X):
+        y_pred = [self._predict(x) for x in X]
+        return y_pred
+
+    
+    def _predict(self, x):
+        posteriors = []
+
+        for idx, _ in enumerate(self._classes):
+            prior = np.log(self._priors[idx])
+            class_conditional = np.sum(np.log(self._pdf(idx, x)))
+            posterior = prior + class_conditional
+            posteriors.append(posterior)
+
+        return self._classes[np.argmax(posteriors)]
+
+
+    def _pdf(self, class_idx, x):
+        mean = self._mean[class_idx]
+        var = self._var[class_idx]
+        numerator = np.exp(- np.power((x - mean), 2) / (2 * var))
+        denominator = np.sqrt(2 * np.pi * var)
+        return numerator / denominator 
